1ee5f5a2bc261530028338c5c9cc2d6adf46cb23
Arpita Gupta <arpitagupta6620@gmail.com>
2023-07-11 14:50:10 +0530

    Made contacts-chat clickable and added time

--- lib/widgets/contacts_list.dart
+++ lib/widgets/contacts_list.dart
@@ -1,37 +1,60 @@
 import 'package:flutter/material.dart';
 import '../info.dart';
+import '../colors.dart';
 
 //16 min
 class ContactsList extends StatelessWidget {
   const ContactsList({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 10),
-      child: ListView.builder(
-        itemCount: info.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(
-              info[index]['name'].toString(),
-              style: const TextStyle(fontSize: 15),
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: info.length,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: ListTile(
+                      title: Text(
+                        info[index]['name'].toString(),
+                        style: const TextStyle(fontSize: 15),
+                      ),
+                      subtitle: Padding(
+                        padding: const EdgeInsets.only(top: 6),
+                        child: Text(
+                          info[index]['message'].toString(),
+                          style: const TextStyle(fontSize: 15),
+                        ),
+                      ),
+                      //Circle Avatar,
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          info[index]['profilePic'].toString(),
+                        ),
+                      ),
+                      //Trailing widget is used to add any widget to list tile in right side,
+                      trailing: Text(
+                        info[index]['time'].toString(),
+                        style:
+                            const TextStyle(fontSize: 13, color: Colors.grey),
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 6),
-              child: Text(
-                info[index]['message'].toString(),
-                style: const TextStyle(fontSize: 15),
-              ),
-            ),
-            //Circle Avatar,
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(
-                info[index]['profilePic'].toString(),
-              ),
-            ),
-          );
-        },
+          ),
+          const Divider(
+            color: dividerColor,
+            indent: 85,
+          )
+        ],
       ),
     );
   }
