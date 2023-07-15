d347e6f3de0869110675e7db75f234a023f5cd6d
Arpita Gupta <arpitagupta6620@gmail.com>
2023-07-15 18:59:06 +0530

    fixed some bugs

--- lib/screens/mobile_chat-screen.dart
+++ lib/screens/mobile_chat-screen.dart
@@ -40,59 +40,51 @@ class MobileChatScreen extends StatelessWidget {
         ],
       ),
       body: Column(
-        //chat list
         children: [
           const Expanded(
             child: ChatList(),
           ),
-          TextField(
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: mobileChatBoxColor,
-              prefixIcon: const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: Icon(
-                  Icons.emoji_emotions_outlined,
-                  color: Colors.grey,
-                ),
-              ),
-              suffixIcon: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: const [
-                    Icon(
-                      Icons.attach_file,
-                      color: Colors.grey,
+          Container(
+            color: mobileChatBoxColor,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                children: const [
+                  Icon(
+                    Icons.emoji_emotions_outlined,
+                    color: Colors.grey,
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Type a message',
+                        hintStyle: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                        ),
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.all(0),
+                      ),
                     ),
-                    Icon(
-                      Icons.currency_rupee,
-                      color: Colors.grey,
-                    ),
-                    Icon(
-                      Icons.camera_alt,
-                      color: Colors.grey,
-                    ),
-                  ],
-                ),
+                  ),
+                  Icon(
+                    Icons.attach_file,
+                    color: Colors.grey,
+                  ),
+                  Icon(
+                    Icons.currency_rupee,
+                    color: Colors.grey,
+                  ),
+                  Icon(
+                    Icons.camera_alt,
+                    color: Colors.grey,
+                  ),
+                ],
               ),
-              hintStyle: const TextStyle(
-                fontSize: 14,
-                color: Colors.grey,
-              ),
-              hintText: 'Type a message',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: const BorderSide(
-                  width: 0,
-                  style: BorderStyle.none,
-                ),
-              ),
-              contentPadding: const EdgeInsets.all(1),
             ),
-          )
+          ),
         ],
-        //text input for message
       ),
     );
   }
